ae8aed5a19b568f611bf101d9fc425c04a587aa7
unknown <camilo2042@hotmail.com>
2022-04-28 17:18:21 -0500

    Solución punto 3

---
+++ T11_P3.js
@@ -0,0 +1,10 @@
+var n1 = prompt("Ingresa un numero:");
+var n2 = prompt("Ingresa otro numero:");
+var n3 = prompt("Ingresa un tercer numero:");
+var suma = parseFloat(n1) + parseFloat(n2);
+var mult = parseFloat(suma) * parseFloat(n3)
+console.log("Primer numero: "+n1);
+console.log("Segundo numero: "+n2);
+console.log("Suman: "+suma);
+console.log("Tercer numero: "+n3);
+console.log("Multiplicaión de la suma por el tercer numero: "+mult);
