cdd8cf27012d0a488e01d2199ddc88ae31352cd8
unknown <camilo2042@hotmail.com>
2022-04-28 17:14:18 -0500

    Solución punto 2

---
+++ T11_P2.js.js
@@ -0,0 +1,6 @@
+var n1 = prompt("Ingresa un numero:");
+var n2 = prompt("Ingresa otro numero:");
+var suma = parseFloat(n1) + parseFloat(n2);
+console.log("Primer numero: "+n1);
+console.log("Segundo numero: "+n2);
+console.log("El resultado de la suma es: "+suma);
